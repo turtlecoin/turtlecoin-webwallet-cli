5a7acc7f1a6269c773d9303c592c746cade4b8e6
fexra <fexra@protonmail.com>
2019-05-08 06:10:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Clutch
+
+Clutch is a browser based CLI wallet for TurtleCoin utilzing xterm, socket.io and turtlecoin-wallet-backend to say the very least.
+
+![Create new Wallet]("/examples/create_wallet.gif")
+
+## Disclaimer
+
+This project is still in its early stage, with little to no functionality.
+
+## Getting Started
+
+```
+# clone
+git clone https://github.com/TurtleCoin/turtlecoin-cli-web-wallet
+
+# change working directory
+cd turtlecoin-cli-web-wallet
+
+# install dependencies
+npm i
+
+# start
+npm start
+
+
+```
---
+++ src/app.ts
@@ -0,0 +1,41 @@
+`use strict`;
+
+import * as express from "express";
+import * as pty from "pty.js";
+import {
+  WalletBackend,
+  ConventionalDaemon,
+  BlockchainCacheApi
+} from "turtlecoin-wallet-backend";
+
+const app = express();
+const expressWs = require("express-ws")(app);
+
+app.use(express.static(`${__dirname}/public`));
+
+// Connect to TurtlePay Cache
+const daemon: BlockchainCacheApi = new BlockchainCacheApi(
+  "blockapi.turtlepay.io",
+  true
+);
+
+// Open wallet
+expressWs.app.ws("/create", async (ws, req) => {
+  const wallet: WalletBackend = WalletBackend.createWallet(daemon);
+
+  ws.send("\r\n");
+
+  ws.send(wallet.getPrimaryAddress());
+  ws.send("\r\n");
+  ws.send("\r\n");
+
+  ws.send(`Private: ${wallet.getPrivateViewKey()}`);
+  ws.send(" \r\n");
+  ws.send(`Public: ${wallet.getSpendKeys(wallet.getPrimaryAddress())}`);
+
+  // Make sure to call stop to let the node process exit
+  wallet.stop();
+});
+
+// Start the server
+app.listen(3000);
